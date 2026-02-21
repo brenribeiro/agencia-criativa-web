02d9eca52aac83e1dcdfa1453900c29c92e4c359
Brenda <assessoriabreribeiro@gmail.com>
2026-02-20 22:19:28 -0300

    atualização do layout e ajustes no js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const toggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
+const navLinks= document.querySelectorAll(" .nav__link");
 
 toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
+  nav.classList.toggle("show");
 });
+
+navLinks.forEach(link => {link.addEventListener("click", () => {
+     nav.classList.remove("show");
+   });
+}); 
